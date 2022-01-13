35730385e951d8fada3b292552ac9f314ed3d979
andressaquintanilha.email <quintanilhalg3@gmail.com>
2022-01-13 00:31:43 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let name = "Andressa Quintanilha"
+
+let year = 30
+
+let currentYear = 2022
+
+let expectedYear = currentYear - year
+
+console.log(`No ano de ${currentYear}, a ${name} tem ${year} anos.`)
+console.log(`A subtração do ano atual (${currentYear}), com a idade da ${name}, resulta no ano de ${expectedYear}.`)
+console.log(`E a soma da idade dela (${year}) com o ano atual (${currentYear}) é igual a ${year + currentYear}.`)
+console.log('Estou mexendo no console.log.')
+
+let quartaFeira = false
+
+console.log(`O dia da semana quarta-feira pertence ao final de semana? R: ${quartaFeira}.`)
